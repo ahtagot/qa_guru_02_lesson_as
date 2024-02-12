83f4fd05609859953d8e8e99310cf5cc53a725d1
astelmakh13 <stelmakh.al@gmail.com>
2024-02-11 18:18:39 -0600

    Removed unnecessary code, removed sleeps, added JS code for removing banners, changed upload function

--- src/test/java/FullTest.java
+++ src/test/java/FullTest.java
@@ -1,12 +1,7 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
-
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -17,12 +12,16 @@ public class FullTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.timeout = 5000;
-        Configuration.holdBrowserOpen = false;
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
     void fillAllFields() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+
         open("/automation-practice-form");
         $("#firstName").setValue("John");
         $("#lastName").setValue("Smith");
@@ -34,24 +33,30 @@ public class FullTest {
         $("#hobbies-checkbox-1").parent().click();
         $("#hobbies-checkbox-2").parent().click();
         $("#hobbies-checkbox-3").parent().click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/landscape.jpg"));
-        sleep(1000);
+        $("#uploadPicture").uploadFromClasspath("landscape.jpg");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").click();
         $(byText("September")).click();
         $(".react-datepicker__year-select").click();
         $(byText("1998")).click();
         $(".react-datepicker__day.react-datepicker__day--013").click();
-        sleep(1000);
-        //String dateValue = $("#dateOfBirthInput").getValue();  experimenting with taking value from date field
-        //System.out.println("Name value: " + dateValue);
         $("#state").click();
         $(byText("NCR")).click();
         $("#city").click();
         $(byText("Delhi")).click();
         $("#submit").click();
         $(".modal-open").shouldBe(visible);
-        $(".modal-content").shouldHave(text("John Smith"), text("j.smith@gmail.com"), text("0123456789"), text("John Smith"), text("Address Somewhere"), text("Male"), text("Chemistry"), text("Sports, Reading, Music"), text("landscape.jpg"), text("NCR Delhi"), text("13 September,1998"));
+        $(".modal-content").shouldHave(text("John Smith"));
+        $(".modal-content").shouldHave(text("j.smith@gmail.com"));
+        $(".modal-content").shouldHave(text("0123456789"));
+        $(".modal-content").shouldHave(text("Male"));
+        $(".modal-content").shouldHave(text("Chemistry"));
+        $(".modal-content").shouldHave(text("Sports, Reading, Music"));
+        $(".modal-content").shouldHave(text("landscape.jpg"));
+        $(".modal-content").shouldHave(text("NCR Delhi"));
+        $(".modal-content").shouldHave(text("13 September,1998"));
+
+
 
 
     }
