7ed9a7cfedd5ba142af7da0830150908568d1305
astelmakh13 <stelmakh.al@gmail.com>
2024-02-27 23:38:32 -0600

    Added negative test

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,125 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import pages.components.CalendarComponent;
+import pages.components.CheckResultComponent;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationPage {
+    private final SelenideElement firstNameInput = $("#firstName");
+    private final SelenideElement lastNameInput = $("#lastName");
+    private final SelenideElement userEmailInput = $("#userEmail");
+    private final SelenideElement userGenderWrapper = $("#genterWrapper");
+    private final SelenideElement userPhoneNumberInput = $("#userNumber");
+    private final SelenideElement calendarInput = $("#dateOfBirthInput");
+    private final SelenideElement userCurrentAddressInput = $("#currentAddress");
+    private final SelenideElement subjectsInput = $("#subjectsInput");
+    private final SelenideElement hobbiesWrapper = $("#hobbiesWrapper");
+    private final SelenideElement pictureUploader = $("#uploadPicture");
+    private final SelenideElement dropListState = $("#state");
+    private final SelenideElement dropListCity = $("#city");
+    private final SelenideElement submitButton = $("#submit");
+    private final SelenideElement checkResult = $(".table-responsive");
+    private final SelenideElement checkResultNegative = $("[class]");
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+    CheckResultComponent checkResultComponent = new CheckResultComponent();
+
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        return this;
+    }
+
+    public RegistrationPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setLastName(String value) {
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setUserEmail(String value) {
+        userEmailInput.setValue(value);
+
+        return this;
+    }
+
+
+    public RegistrationPage setGender(String value) {
+        userGenderWrapper.$(byText(value)).click();
+        return this;
+    }
+
+    public RegistrationPage setUserPhoneNumber(String value) {
+        userPhoneNumberInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage setUserCurrentAddress(String value) {
+        userCurrentAddressInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage setDateOfBirth(String day, String month, String year) {
+        calendarInput.click();
+        calendarComponent.setDate(day, month, year);
+        return this;
+    }
+
+    public RegistrationPage setUserSubjects(String value) {
+        subjectsInput.setValue(value).pressEnter();
+        return this;
+    }
+
+    public RegistrationPage setHobbies(String value) {
+        hobbiesWrapper.$(byText(value)).click();
+        return this;
+    }
+
+    public RegistrationPage uploadPicture(String pictureName) {
+        pictureUploader.uploadFromClasspath(pictureName);
+        return this;
+    }
+
+    public RegistrationPage selectState(String state) {
+        dropListState.click();
+        dropListState.$(byText(state)).click();
+        return this;
+    }
+
+    public RegistrationPage selectCity(String city) {
+        dropListCity.click();
+        dropListCity.$(byText(city)).click();
+        return this;
+    }
+
+    public RegistrationPage clickSubmitButton() {
+        submitButton.click();
+        return this;
+
+    }
+
+    public RegistrationPage checkResult(String key, String value) {
+        checkResultComponent.getResultTable(key, value);
+
+        return this;
+    }
+
+    public void checkResultTableIsNotVisible() {
+        checkResult.shouldNotBe(visible);
+
+    }
+
+
+}
