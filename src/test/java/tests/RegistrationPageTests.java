ae8400b1032e0e2161a46f1e70e0a36beb97b478
astelmakh13 <stelmakh.al@gmail.com>
2024-03-27 16:10:05 -0500

    added: fixed typo

--- src/test/java/tests/RegistrationPageTests.java
+++ src/test/java/tests/RegistrationPageTests.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
 import static data.TestData.*;
-@Tag("demoqa-run")
+@Tag("demoqa_run")
 public class RegistrationPageTests extends TestBase  {
 
     RegistrationPage registrationPage = new RegistrationPage();
